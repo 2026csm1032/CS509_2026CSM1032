6c29267750c8abfa0dcb689cc7c0d0424523512b
2026csm1032 <2026csm1032@iitrpr.ac.in>
2026-08-09 00:37:04 +0530

    added csr conversion for weighted graphs

--- assignment_1/src/csr.h
+++ assignment_1/src/csr.h
@@ -6,4 +6,7 @@ using namespace std;
 
 void convertToCSR( vector<vector<int>>& adj, vector<int>& row_ptr, vector<int>& col_idx);
 
+void convertToCSR(vector<vector<pair<int,int>>>& adj,vector<int>& row_ptr,vector<int>& col_idx,vector<int>& weights);
+
+
 #endif
